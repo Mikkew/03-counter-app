75ca06bcddc1e56c927c07ea52ddf328dc06d610
mmedina <miguel.medina@meganucleo.mx>
2020-12-22 22:04:11 -0600

    Unidad 3 de Testing

---
+++ src/test/CounterApp.test.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { shallow } from 'enzyme';
+import CounterApp from '../CounterApp';
+import "@testing-library/jest-dom";
+
+describe('Pruebas en el <CounterApp />', () => {
+
+  let wrapper = shallow( <CounterApp />);
+
+
+  beforeEach( () => {
+    wrapper = shallow( <CounterApp />);
+  })
+
+  test('debe de mostrar <CounterApp /> correctamente', () => {
+    
+    const valor = 1;
+    const wrapper = shallow( <CounterApp value={valor} />);
+
+    expect( wrapper ).toMatchSnapshot();
+  })
+  
+  test('debe de mostrar el valor por defecto de 100', () => {
+
+    const valor = 100;
+    const wrapper = shallow( <CounterApp value={valor} />);
+
+    const valorDefecto = wrapper.find('h2').text().trim();
+    console.log( valorDefecto );
+
+    expect( parseInt(valorDefecto) ).toBe(100)
+  })
+
+  test('debe de incrementar con el boton de +1', () => {
+    
+    wrapper.find('button').at(0).simulate('click');
+    const counterText = wrapper.find('h2').text().trim();
+    expect( counterText ).toBe('11');
+    console.log(counterText);
+
+  })
+  
+  test('debe de incrementar con el boton de -1', () => {
+    
+    wrapper.find('button').at(2).simulate('click');
+    const counterText = wrapper.find('h2').text().trim();
+    expect( counterText ).toBe('9');
+    console.log(counterText);
+
+  })
+  
+  test('debe de colocar un valor por defecto en btn reset', () => {
+
+    const wrapper = shallow( <CounterApp value={ 105 } />);
+    wrapper.find('button').at(0).simulate('click');
+    const counterText = wrapper.find('h2').text().trim();
+
+    console.log( counterText );
+
+    expect(counterText).toBe('105')
+  })
+
+})
--- src/test/PrimeraApp.test.js
+++ src/test/PrimeraApp.test.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { render } from "@testing-library/react";
+import React from 'react';
+// import { render } from "@testing-library/react";
 import '@testing-library/jest-dom';
 import PrimeraApp from "../PrimeraApp";
 import { shallow } from 'enzyme';
@@ -17,7 +17,23 @@ describe('Pruebas en mi <PrimeraApp />', () => {
     
     const saludo = "Hola, Soy Jose";
     const wrapper = shallow( <PrimeraApp saludo={ saludo } /> );
+
+    expect(  wrapper ).toMatchSnapshot();
+
   })
   
+  test('debe de mostrar el subtitulo enviador por props', () => {
+    
+    const saludo = "Hola, Soy Jose";
+    const subtitulo = "El merengues";
+    const wrapper = shallow( <PrimeraApp saludo={ saludo } subtitulo={ subtitulo } /> );
+
+    const textoParrafo = wrapper.find('p').text()
+    console.log(textoParrafo);
+
+    expect( textoParrafo ).toBe( subtitulo );
+  })
   
+
 })
+
